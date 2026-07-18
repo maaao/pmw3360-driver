f7d47bb1fcfd29cdcb2274b3d10087fda73b3b57
maaao <b.b.ambi27@gmail.com>
2026-07-18 12:14:10 +0900

    出力ログを追加

--- src/pmw3360.c
+++ src/pmw3360.c
@@ -497,6 +497,7 @@ static int pmw3360_async_init_fw_load_verify(const struct device *dev) {
 
     LOG_DBG("Optical chip firmware ID: 0x%x", fw_id);
     if (fw_id != PMW3360_FIRMWARE_ID) {
+        LOG_ERR("SROM ID read = 0x%02x expected=0x%02x", fw_id, PMW3360_FIRMWARE_ID);
         LOG_ERR("Chip is not running from SROM!");
         return -EIO;
     }
